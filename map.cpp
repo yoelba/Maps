eb87c72b506835b180be79cb744a0a088dbc9b32
nahomjd <jackson.nahom@gmail.com>
2017-04-26 20:43:10 -0400

    Add files via upload

--- map.cpp
+++ map.cpp
@@ -1,39 +1,9 @@
-/*
-
-CONSULT CHAPTER 12 OF CS142 NOTES - EXCELLENT RESCOURCE!
- -remember to compile with -std=c++11 flag
-
-TO DO: 
-
- x Default constructor --> 'x' means this is already done
- o Copy constructor - DEEP COPY
- o Destructor - DEALLOCATE MEMORY
- o Indexing operator	
- o size
- o empty
- o begin
- o end
- o find
- o erase(key)
- o erase(iterator)
- o clear
- o swap
- o Iterators - that support the following operations
-	o default constructor --> how tf is this done????????
-	o * - RETURN REFERENCE TO PAIR OBJECT - so a reference to it's node place in the map?
-	x ++
-	x ==
-	x !=
-*/
-
 #include <iostream>
 #include <string>
-#include <iterator>
-
 using namespace std;
 
 template <class K, class V>
-class Map; 
+class Map;
 
 template <class K, class V>
 class Node{
@@ -42,86 +12,58 @@ class Node{
 
 	private : //Constructors are private, but Map will be able to access them. (And only Maps should ever instantiate nodes as far as I know)
 	Node() :
-		key (), value(), left(NULL), right(NULL), parent(NULL) {}
-	
+		key (), value(), left(nullptr), right(nullptr), parent(nullptr) {}
+
 	Node(const K & k, const V & v, Node * l, Node * r, Node * p) :
 		key(k), value(v), left(l), right(r), parent(p) {}
-	
+
 	K key;
 	V value;
 
-	Node<K, V> * parent;	
+	Node<K, V> * parent;
 	Node<K, V> * left;
 	Node<K, V> * right;
 };
 
 template <class K, class V>
 class Map{
-
 	public:
-	Map() :  //Default constructor should create a map without instantiating it's root node (Don't need a dummy node like in CS142's list implementation per Maciel)
-		root () {} // by calling the root 
-	~Map(); // Destructor
-	bool operator==(Map<K, V> & mapTwo); 
-	Node<K, V> * operator++(Map<K, V> & mapTwo); // return type correct?
-	bool operator!=(Map<K, V> & mapTwo);
-		
-	
+	Map() :
+            root () {}//Default constructor should create a map without instantiating it's root node (Don't need a dummy node like in CS142's list implementation per Maciel)
+    Map(const K & Key0, const V & Value0) : Key(Key0), Value(Value0) {}
+
+    int Size() const { return size_; }
+    V find(V x, K key){
+        //while
+
+    }
+
+    //V & operator[]
+//		root () {}
 	private:
-	Node<K, V> * root; //Yes, this is the only node the object contains! root, however, will maintain it's own 'left'/'right' pointers towards the rest of the nodes
+	Node<K, V> * root;  //Yes, this is the only node the object contains! root, however, will maintain it's own 'left'/'right' pointers towards the rest of the nodes
+
+    K Key;
+    V Value;
+    int size_;
 };
 
-// Operator ==
-template<class K, class V>
-inline bool Map<K, V>::operator==(Map<K, V> & mapTwo){
-
-	return ((root->key == mapTwo.root->key) && 
-		(root->value == mapTwo.root->value));
-}
-
-// Operator ++
-template<class K, class V>
-inline Node<K, V> * Map<K, V>::operator++(Map<K, V> & myMap){
-	
-	return myMap = myMap->right;
-}
-
-// Operator !=
-template<class K, class V>
-inline bool Map<K, V>::operator!=(Map<K, V> & mapTwo){
-	
-	return ((root->key != mapTwo.root->key) || 
-		(root->value != mapTwo.root->value));
-}
-
-// Destructor
-template <class K, class V>
-inline Map<K, V>::~Map(){
-
-	Map<K, V> * m = root->right;
-	for(auto it = m.begin(); m.end(); ++it){
-	// since we have to implement begin and end, should we use
-	// those implemented versions here or nah?
-		m = m->right;
-		delete m->left;	
-	}
-	delete * root;
-}
 
 int main(){
+	Map<int, string> defaultMap();
+	Map<char,std::string> mymap;
 
-	Map<int, string> myMap(); // explain why this needs '()' for me
-	// how do we insert values into this thing??		
+   /*mymap['a']="an element";
+    mymap['b']="another element";
+    mymap['c']=mymap['b'];
+
+    std::cout << "mymap['a'] is " << mymap['a'] << '\n';
+    std::cout << "mymap['b'] is " << mymap['b'] << '\n';
+    std::cout << "mymap['c'] is " << mymap['c'] << '\n';
+    std::cout << "mymap['d'] is " << mymap['d'] << '\n';
+
+    std::cout << "mymap now contains " << mymap.Size() << " elements.\n";
+    */
 
 	return 0;
 };
-
-
-
-
-
-
-
-
-
-
