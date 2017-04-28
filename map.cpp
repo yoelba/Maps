f5fa6b4d07779265b30709d16435b69864ebc6b9
nahomjd <jackson.nahom@gmail.com>
2017-04-27 20:45:42 -0400

    Add files via upload

--- map.cpp
+++ map.cpp
@@ -1,10 +1,42 @@
+/*
+CONSULT CHAPTER 12 OF CS142 NOTES - EXCELLENT RESCOURCE!
+ -remember to compile with -std=c++11 flag
+TO DO:
+LEGEND: o == Not done, x == done (to our knowledge), A = ask Maciel, T = test this, it looks a little funny
+ x Default constructor --> 'x' means this is already done
+ A Copy constructor - DEEP COPY
+ A Destructor - DEALLOCATE MEMORY --> how is this different from clear??
+ o Indexing operator - this is insert
+ x size --> we should increment this with every new insert??
+ x empty
+ x/T begin
+ x/T end
+ x/T find
+ o erase(key) --> I started this
+ o erase(iterator) --> I started this
+ x clear --> traverses tree and deletes it all
+ o swap --> shouldn't be too hard
+ x Iterators - that support the following operations
+	x default constructor
+	x * - RETURN REFERENCE TO PAIR OBJECT
+	x ++ We did this wrong - see notes below.
+	x ==
+	x !=
+*/
+
 #include <iostream>
 #include <string>
-using namespace std;
+#include <iterator>
+
+//using namespace std;
+
+template <class K, class V>
+class Iterator;
 
 template <class K, class V>
 class Map;
 
+/////////////////////////////////////////////////////////////////////////
 template <class K, class V>
 class Node{
 
@@ -12,7 +44,7 @@ class Node{
 
 	private : //Constructors are private, but Map will be able to access them. (And only Maps should ever instantiate nodes as far as I know)
 	Node() :
-		key (), value(), left(nullptr), right(nullptr), parent(nullptr) {}
+		key (), value(), left(NULL), right(NULL), parent(NULL) {}
 
 	Node(const K & k, const V & v, Node * l, Node * r, Node * p) :
 		key(k), value(v), left(l), right(r), parent(p) {}
@@ -25,45 +57,145 @@ class Node{
 	Node<K, V> * right;
 };
 
+/////////////////////////////////////////////////////////////////////////
 template <class K, class V>
 class Map{
+	typedef Iterator<K, V> iterator;
+
 	public:
-	Map() :
-            root () {}//Default constructor should create a map without instantiating it's root node (Don't need a dummy node like in CS142's list implementation per Maciel)
-    Map(const K & Key0, const V & Value0) : Key(Key0), Value(Value0) {}
+	Map() : root () {} // instantiating our head node for the map
+    Map(const Map & m) : Map(root->left, root->right) {}
+	~Map(); // Destructor --> implement this!
 
-    int Size() const { return size_; }
-    V find(V x, K key){
-        //while
+	bool empty() { return (root == NULL); }
 
-    }
+	iterator begin(Node<K, V> & root){ // MIN operation, fixed
+	// on second thought, the STL version doesn't have any args... we should just
+	// call root cause we have it as a private data member
+		while(root->left != nullptr){
+			root = root->left;
+		}
+		return root;
+	}
 
-    //V & operator[]
-//		root () {}
+	iterator end(Node<K, V> maxNode){ return iterator(nullptr); } // Maciel's way :( but fixed
+	// or should this go all the way to the right, then when root->right is a nullptr, you return that?
+//		maxNode = root;
+//		while(root->next != nullptr) root = root->next;
+//		return root->next;
+
+	iterator find(Node<K, V> & root, K k){
+		while((root != NULL) && (root->key != k)){
+			if(k < root->key) root = root->left;
+			else root = root->right;
+		}
+		return root;
+	}
+
+	iterator clear(){
+		if(root != NULL){
+			(root->left).clear();
+			(root->right).clear();
+			delete(root);
+			root = NULL;
+		}
+	}
+
+	// erase using a nodes key
+	iterator erase(Node<K, V> & root, K k){
+		while(root->key != k){
+			if(k < root->key) root = root->left;
+			else root = root->right;
+		}
+		// this is where you do some bullshit
+	}
+
+	// erase using a nodes iterator... place in the tree?
+	iterator erase(Node<K, V> & root, Iterator<K, V> itr){
+		// yeah not really sure
+	}
+
+	int size() { return size_; }
+
 	private:
-	Node<K, V> * root;  //Yes, this is the only node the object contains! root, however, will maintain it's own 'left'/'right' pointers towards the rest of the nodes
-
-    K Key;
-    V Value;
+	Node<K, V> * root; //this is the only node the object contains
     int size_;
 };
 
+/////////////////////////////////////////////////////////////////////////
+template <class K, class V>
+class Iterator{
 
+	friend class Map<K, V>;
+
+	public:
+	Iterator() : p_current_node(nullptr) {}
+
+	bool operator==(const Iterator & itr2) const{
+		return (p_current_node == itr2.p_current_node);
+	}
+
+	bool operator!=(const Iterator & itr2) const{
+		return (p_current_node != itr2.p_current_node);
+	}
+
+	Iterator<K, V> * operator++(Node<K, V> & currNode){ // successor operation, fixed
+                if(currNode->right != NULL) return begin(currNode->right);
+                else{
+                        Node<K, V> parentNode = currNode->parent;
+                        while((parentNode != NULL) && (currNode = parentNode->right)){
+                                currNode = parentNode;
+                                parentNode = currNode->parent;
+                        }
+                        return parentNode;
+                }
+        }
+
+	std::pair<K, V> & operator * () const{
+		return std::pair<K, V>(p_current_node->key, p_current_node->value);
+	}
+
+	private:
+	explicit Iterator(Node<K, V> * p ) : p_current_node(p) {}
+
+	Node<K, V> * p_current_node;
+
+};
+
+/////////////////////////////////////////////////////////////////////////
 int main(){
-	Map<int, string> defaultMap();
-	Map<char,std::string> mymap;
 
-   /*mymap['a']="an element";
-    mymap['b']="another element";
-    mymap['c']=mymap['b'];
+    Map<char,int> mymap;
 
-    std::cout << "mymap['a'] is " << mymap['a'] << '\n';
-    std::cout << "mymap['b'] is " << mymap['b'] << '\n';
-    std::cout << "mymap['c'] is " << mymap['c'] << '\n';
-    std::cout << "mymap['d'] is " << mymap['d'] << '\n';
+    mymap['b'] = 100;
+    mymap['a'] = 200;
+    mymap['c'] = 300;
 
-    std::cout << "mymap now contains " << mymap.Size() << " elements.\n";
-    */
+        // show content:
+    for (Map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
+        std::cout << it->first << " => " << it->second << '\n';
+
+    mymap['a']=10;
+    mymap['b']=20;
+    mymap['c']=30;
+
+    while (!mymap.empty())
+    {
+        std::cout << mymap.begin()->first << " => " << mymap.begin()->second << '\n';
+        mymap.erase(mymap.begin());
+    }
+
+    it = mymap.find('b');
+    if (it != mymap.end())
+    mymap.erase (it);
+
+    // print content:
+    std::cout << "elements in mymap:" << '\n';
+    std::cout << "a => " << mymap.find('a')->second << '\n';
+    std::cout << "c => " << mymap.find('c')->second << '\n';
+    std::cout << "d => " << mymap.find('d')->second << '\n';
 
 	return 0;
 };
+
+
